eac1a03723ef2c121e5fde08c762882c99ac16df
Euglena <shimidorimu.00@gmail.com>
2023-10-23 22:23:44 +0900

    Config.javaで一括破壊上限を設定

---
+++ src/main/java/com/bulkbreakmod/Config.java
@@ -0,0 +1,24 @@
+package com.bulkbreakmod;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+
+public class Config {
+    public static ForgeConfigSpec CONFIG;
+
+    public static ForgeConfigSpec.IntValue MAX_BREAK_LIMIT;
+
+    static {
+        ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
+
+        builder.push("General");
+        MAX_BREAK_LIMIT = builder.comment("最大一括破壊数")
+                .defineInRange("maxBreakLimit", 10, 1, 100);
+        builder.pop();
+
+        CONFIG = builder.build();
+    }
+
+    public static void loadConfig() {
+        // ここでコンフィグファイルから設定を読み込む
+    }
+}
